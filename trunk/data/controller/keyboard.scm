abc75e4087e8f278ff2c9c7d756ca068c30fc76b
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-06-25 23:51:23 +0000

    - added new walk and run

--- trunk/data/controller/keyboard.scm
+++ trunk/data/controller/keyboard.scm
@@ -3,6 +3,7 @@
  (down  (keyboard-button "down"))
  (left  (keyboard-button "left"))
  (right (keyboard-button "right"))
+ (run  (keyboard-button "e"))
  (fire  (keyboard-button "lcontrol"))
  (jump  (keyboard-button "up"))
  )
