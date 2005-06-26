37986820c8bd89290414e87d9cc907e9c8520e3e
jamesgregory <jamesgregory@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-06-26 17:18:55 +0000

    lcontrol doesn't work in Windows

--- trunk/data/controller/keyboard.scm
+++ trunk/data/controller/keyboard.scm
@@ -4,7 +4,7 @@
  (left  (keyboard-button "left"))
  (right (keyboard-button "right"))
  (run  (keyboard-button "e"))
- (fire  (keyboard-button "lcontrol"))
+ (fire  (keyboard-button "space"))
  (jump  (keyboard-button "up"))
  )
  
