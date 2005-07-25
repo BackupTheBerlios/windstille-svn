f9963f85dc3fbb3887b91f870a030911559e45e1
jamesgregory <jamesgregory@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-07-25 10:27:56 +0000

    Remove obsolete dialog.nut stuff

--- trunk/src/character.cpp
+++ trunk/src/character.cpp
@@ -1,4 +1,4 @@
-//  $Id: animation_obj.hpp,v 1.3 2003/09/12 16:31:20 grumbel Exp $
+//  $Id$
 // 
 //  Windstille - A Jump'n Shoot Game
 //  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
@@ -27,8 +27,8 @@
 #include "physfs/physfs_stream.hpp"
 #include "console.hpp"
 #include "sprite3d/manager.hpp"
-#include "util.hpp"
 #include "lisp/properties.hpp"
+#include "util.hpp"
 #include "windstille_getters.hpp"
 
 #include <exception>
@@ -70,20 +70,13 @@ Character::use()
 {
   //Scripting::add_dialog(Dialog::TOP | Dialog::RIGHT, "human/portrait");
 
-  //first add standard dialog functions
-  std::string filename = ::datadir + "scripts/" + "dialog.nut";
-  std::string script;
-  file_to_string(filename, script);
-  
-  //then add this character's dialog script
-  filename = basename(GameSession::current()->get_filename());
+  std::string filename = basename(GameSession::current()->get_filename());
   filename.erase(filename.find('.'));
-  filename = ::datadir + "scripts/" + filename + "/" + name + ".nut";
-  file_to_string(filename, script);
+  filename = "scripts/" + filename + "/" + name + ".nut";
 
   try 
     {
-      script_manager->run_script(script, name);
+      script_manager->run_script_file(filename);
     } catch (std::exception e) {
       console << e.what() << std::endl;
     }
