ddf2e99aeee6efc97a131bae3494d60cf048cb51
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-08-04 21:11:31 +0000

    - removed some debug couts - added hardcoded joystick support

--- trunk/src/input/input_manager_sdl.cpp
+++ trunk/src/input/input_manager_sdl.cpp
@@ -28,9 +28,16 @@
 
 InputManagerSDL* InputManagerSDL::current_ = 0;
 
+#define DEAD_ZONE 4096
+
 InputManagerSDL::InputManagerSDL()
 {
   current_ = this;
+  
+  int num_joysticks = SDL_NumJoysticks();
+
+  if (num_joysticks > 0)
+    /*SDL_Joystick* joy =*/ SDL_JoystickOpen(0);
 }
 
 InputManagerSDL::~InputManagerSDL()
@@ -50,12 +57,10 @@ InputManagerSDL::on_event(const SDL_Event& event)
             if ((event.key.keysym.unicode > 0 && event.key.keysym.unicode < 128)
                 && (isgraph(event.key.keysym.unicode) || event.key.keysym.unicode == ' '))
               {
-                std::cout << "Letter: '" << event.key.keysym.unicode << "'" << std::endl;
                 add_keyboard_event(0, KeyboardEvent::LETTER, event.key.keysym.unicode);
               }
             else
               {
-                std::cout << "Special: '" << event.key.keysym.sym << "'" << std::endl;
                 add_keyboard_event(0, KeyboardEvent::SPECIAL, event.key.keysym.sym);
               }
           }
@@ -114,7 +119,42 @@ InputManagerSDL::on_event(const SDL_Event& event)
       break;
 
     case SDL_JOYAXISMOTION:
-      // event.jaxis
+      if (event.jaxis.axis == 0) 
+        {
+          if (event.jaxis.value < -DEAD_ZONE)
+            {
+              add_button_event(LEFT_BUTTON, true);   
+              add_button_event(RIGHT_BUTTON, false);
+            }
+          else if (event.jaxis.value > DEAD_ZONE)
+            {
+              add_button_event(LEFT_BUTTON, false); 
+              add_button_event(RIGHT_BUTTON, true); 
+            }
+          else
+            {
+              add_button_event(LEFT_BUTTON, false);
+              add_button_event(RIGHT_BUTTON, false);
+            }
+        }
+      else if (event.jaxis.axis == 1) 
+        {
+          if (event.jaxis.value < -DEAD_ZONE)
+            {
+              add_button_event(UP_BUTTON, true);   
+              add_button_event(DOWN_BUTTON, false);
+            }
+          else if (event.jaxis.value > DEAD_ZONE)
+            {
+              add_button_event(UP_BUTTON, false); 
+              add_button_event(DOWN_BUTTON, true); 
+            }
+          else
+            {
+              add_button_event(UP_BUTTON, false);
+              add_button_event(DOWN_BUTTON, false);
+            }
+        }
       break;
 
     case SDL_JOYBALLMOTION:
@@ -126,11 +166,23 @@ InputManagerSDL::on_event(const SDL_Event& event)
       break;
           
     case SDL_JOYBUTTONUP:
-      // event.jbutton
-      break;
-
     case SDL_JOYBUTTONDOWN:
-      // event.jbutton
+      if (event.jbutton.button == 0)
+        {
+          add_button_event(FIRE_BUTTON, event.jbutton.state); 
+        }
+      else if (event.jbutton.button == 1)
+        {
+          add_button_event(USE_BUTTON, event.jbutton.state); 
+        }
+      else if (event.jbutton.button == 2)
+        {
+          add_button_event(JUMP_BUTTON, event.jbutton.state);
+        }
+      else if (event.jbutton.button == 3)
+        {
+          add_button_event(RUN_BUTTON, event.jbutton.state);
+        }
       break;
 
     default:
