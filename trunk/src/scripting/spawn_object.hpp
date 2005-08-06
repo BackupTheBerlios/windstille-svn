3baba81946177a0200135af3f11c39243129db0d
jamesgregory <jamesgregory@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-08-06 16:07:58 +0000

    correct doc comment

--- trunk/src/scripting/spawn_object.hpp
+++ trunk/src/scripting/spawn_object.hpp
@@ -13,7 +13,7 @@ namespace Scripting
  *    name:    string with name of object
  *    table:   table that is parsed to get object properties
  *
- * Example: spawn_object("ScriptableObject", { name="blup", x = 2.0, y = 5.0, sprite="arrows/red" });
+ * Example: spawn_object("scriptable-object", {name="blup", pos=[350,370], sprite="images/arrow.sprite"});
  */
 int spawn_object(HSQUIRRELVM v) __custom;
 
