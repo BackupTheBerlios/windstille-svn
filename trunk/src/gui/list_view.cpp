d529a7ce5aa850ce36aa2d874f7df9ae6705f428
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-08-21 22:47:16 +0000

    - added listview (just api, no code)

---
+++ trunk/src/gui/list_view.cpp
@@ -0,0 +1,66 @@
+/*  $Id$
+**   __      __ __             ___        __   __ __   __
+**  /  \    /  \__| ____    __| _/_______/  |_|__|  | |  |   ____
+**  \   \/\/   /  |/    \  / __ |/  ___/\   __\  |  | |  | _/ __ \
+**   \        /|  |   |  \/ /_/ |\___ \  |  | |  |  |_|  |_\  ___/
+**    \__/\  / |__|___|  /\____ /____  > |__| |__|____/____/\___  >
+**         \/          \/      \/    \/                         \/
+**  Copyright (C) 2005 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU General Public License
+**  as published by the Free Software Foundation; either version 2
+**  of the License, or (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+** 
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+**  02111-1307, USA.
+*/
+
+#include "list_view.hpp"
+
+namespace GUI {
+
+ListView::ListView(const Rectf& rect, Component* parent)
+  : Component(rect, parent)
+{
+  current_item = 0;
+}
+
+ListView::~ListView()
+{
+}
+
+void
+ListView::draw()
+{
+  
+}
+
+void
+ListView::update(float delta, const Controller& controller)
+{
+  
+}
+
+void
+ListView::add_column(const std::string& name)
+{
+  headers.push_back(name);
+}
+
+void
+ListView::add_item(const Item& item)
+{
+  items.push_back(item);
+}
+
+} // namespace GUI
+
+/* EOF */
