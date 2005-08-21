c148c53447244eecbd303524fd62af1958e6bb0a
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-08-21 23:56:08 +0000

    - added textview, again only empty class placeholder

---
+++ trunk/src/gui/text_view.cpp
@@ -0,0 +1,56 @@
+/*  $Id$
+**   __      __ __             ___        __   __ __   __
+**  /  \    /  \__| ____    __| _/_______/  |_|__|  | |  |   ____
+**  \   \/\/   /  |/    \  / __ |/  ___/\   __\  |  | |  | _/ __ \
+**   \        /|  |   |  \/ /_/ |\___ \  |  | |  |  |_|  |_\  ___/
+**    \__/\  / |__|___|  /\____ /____  > |__| |__|____/____/\___  >
+**         \/          \/      \/    \/                         \/
+**  Copyright (C) 2005 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU General Public License
+**  as published by the Free Software Foundation; either version 2
+**  of the License, or (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+** 
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+**  02111-1307, USA.
+*/
+
+#include "text_view.hpp"
+
+namespace GUI {
+
+TextView::TextView(const Rectf& rect, Component* component)
+  : Component(rect, component)
+{
+}
+
+TextView::~TextView()
+{
+}
+  
+void
+TextView::draw()
+{
+}
+
+void
+TextView::update(float delta, const Controller& controller)
+{
+}
+
+void
+TextView::set_text(const std::string& text)
+{
+}
+
+} // namespace GUI
+
+/* EOF */
