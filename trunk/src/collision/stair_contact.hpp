919cfc223d23eec87cec2239b721c1a7f95ad58c
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2005-08-30 18:01:34 +0000

    - only empty placeholder for now

---
+++ trunk/src/collision/stair_contact.hpp
@@ -0,0 +1,48 @@
+/*  $Id$
+**   __      __ __             ___        __   __ __   __
+**  /  \    /  \__| ____    __| _/_______/  |_|__|  | |  |   ____
+**  \   \/\/   /  |/    \  / __ |/  ___/\   __\  |  | |  | _/ __ \
+**   \        /|  |   |  \/ /_/ |\___ \  |  | |  |  |_|  |_\  ___/
+**    \__/\  / |__|___|  /\____ /____  > |__| |__|____/____/\___  >
+**         \/          \/      \/    \/                         \/
+**  Copyright (C) 2005 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU General Public License
+**  as published by the Free Software Foundation; either version 2
+**  of the License, or (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+** 
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+**  02111-1307, USA.
+*/
+
+#ifndef HEADER_STAIR_CONTACT_HPP
+#define HEADER_STAIR_CONTACT_HPP
+
+#include "contact.hpp"
+
+/** A special contact to walk stairs up or down */
+class StairContact : public Contact
+{
+private:
+public:
+  StairContact();
+  
+  void set_velocity(float a);
+  void update(float delta);
+
+private:
+  StairContact (const StairContact&);
+  StairContact& operator= (const StairContact&);
+};
+
+#endif
+
+/* EOF */
