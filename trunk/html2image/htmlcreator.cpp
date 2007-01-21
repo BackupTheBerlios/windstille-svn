59f3e92a4f35cfadb7bd4e8b29add53bf7c9bea3
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2007-01-21 22:07:07 +0000

    - changed code so that it can handle pages larger then 16384

--- trunk/html2image/htmlcreator.cpp
+++ trunk/html2image/htmlcreator.cpp
@@ -1,9 +1,13 @@
+#include <vector>
 #include <iostream>
 #include <time.h>
 #include <kapp.h>
 #include <kapplication.h>
 #include <kcmdlineargs.h>
+#define private public
 #include <khtml_part.h>
+#include <khtmlview.h>
+#undef  private 
 #include <kuniqueapplication.h>
 #include <qimage.h>
 #include <qpainter.h>
@@ -11,6 +15,8 @@
 #include <qpixmap.h>
 #include <qvbox.h>
 
+#include "khtml/rendering/render_canvas.h"
+#include "khtml/rendering/render_layer.h"
 #include "htmlcreator.h"
 
 QCString output_file;
@@ -33,12 +39,29 @@ void trim(QImage& img)
     }
 }
 
+int last_empty_line(QImage& img)
+{
+  for(int i = img.height()-1; i >= 0; --i)
+    {
+      uchar* p  = img.scanLine(i);
+      int width = img.width();
+      for(int x = 0; x < 4*width; ++x)
+        {
+          if (p[x] != 255)
+            {
+              return i+1;
+            }
+        }
+    }  
+  return 0;
+}
+
 HTMLCreator::HTMLCreator(int width, int heigth)
   : KMainWindow(0L, "HTMLCreator")
 {
   QVBox * vbox = new QVBox ( this );
 
-  m_html = new KHTMLPart(vbox);
+  m_html = new KHTMLPart;//(vbox);
   connect(m_html, SIGNAL(completed()), this, SLOT(save()));
   m_html->setJScriptEnabled(false);
   m_html->setJavaEnabled(false);
@@ -48,6 +71,7 @@ HTMLCreator::HTMLCreator(int width, int heigth)
   m_html->setZoomFactor(100);
   //m_html->getHeight();
   //m_html->resize(480,640);
+  m_html->view()->resize(480,640);
   
   m_html->openURL(input_file);
   std::cout << "Opening: " << input_file << std::endl;
@@ -61,45 +85,62 @@ HTMLCreator::HTMLCreator(int width, int heigth)
 
 HTMLCreator::~HTMLCreator()
 {
-    delete m_html;
+  delete m_html;
 }
 
 void HTMLCreator::save()
 {
-  int start_height = 2048;
-
   QImage  img;
+  bool more = false;
+  int yoff = 0;
+  int last_empty = 1; 
+  
+  std::vector<QImage> images;
   do
     {
-      start_height *= 2;
-
       QPixmap pix;
-      pix.resize(480, start_height);
+      pix.resize(480, 2048);
       pix.fill( QColor( 255, 255, 255 ));
-      QRect rc(0, 0, pix.width(), pix.height());
-
+      //QRect rc(0, -yoff, pix.width(), pix.height()+yoff);
+      QRect rc(0, -yoff, pix.width(), pix.height()+yoff);
       QPainter p;
       ////p.setRenderHints()
       p.begin(&pix);
-      m_html->paint(&p, rc);
+      m_html->paint(&p, rc, yoff, &more);
       p.end();
+
+      yoff += pix.height();
   
       img = pix.convertToImage();
 
-      trim(img);
-      std::cout << "Got " << start_height << " trim to " << img.height() << std::endl;
+      last_empty = last_empty_line(img);
+
+      images.push_back(img.copy(0, 0, img.width(), 
+                                std::max(0, std::min(last_empty + 6, img.height()))));
     }
-  while(img.height() > start_height - 100 && !(start_height > img.height()) && start_height < 16384);
-  
-  if (img.save(output_file, "JPEG", 80))
+  while(last_empty != 0); //(img.height() > start_height - 100 && !(start_height > img.height()) && start_height < 16384);
+      
+  std::cout << "Got " << images.size() << " pages" << std::endl;
+
+  int img_width  = 480;
+  int img_height = 0;
+  for(int i = 0; i < (int)images.size(); ++i)
+    img_height += images[i].height();
+
+  QImage total(img_width, img_height, 32);
+
+  int y = 0;
+  for(int i = 0; i < (int)images.size(); ++i)
     {
-      std::cout << "Save to: " << output_file << std::endl;
+      bitBlt(&total, 0, y, &images[i], 0, 0, -1, -1, 0);
+      y += images[i].height();
     }
-  else
-    {
-      std::cout << "Save: not done" << std::endl;
-    }
+
+  //img = img.copy(0, 0, img.width(), std::max(272, last_empty));
   
+  total.save(output_file, "JPEG", 80);
+  std::cout << "Outfile: " << output_file << std::endl;
+
   // Brute force!
   exit(0);
 }
@@ -108,12 +149,12 @@ static const KCmdLineOptions options[] =
   {
     { "output <file>", "Output file", 0 },
     { "file <file>",   "A required argument 'file'", 0 },
-    NULL
+    { NULL }
   };
   
 int main(int argc, char** argv)
 {
-  std::cout << "Relative paths don't work!" << std::endl;
+  //std::cout << "Relative paths don't work!" << std::endl;
   KCmdLineArgs::init(argc, argv, "htmlcreator", "htmlcreator", "Converts HTML to PNG", "0.0.0");
   KCmdLineArgs::addCmdLineOptions( options );
   KUniqueApplication::addCmdLineOptions();
