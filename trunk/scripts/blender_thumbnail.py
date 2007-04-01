d6f6b20403591faac7661c34130182a36b018ac3
grumbel <grumbel@05ad5834-82cd-0310-9e85-c1570309bcb5>
2007-04-01 19:07:55 +0000

    - simplified code a bit - added free rotation around the object

--- trunk/scripts/blender_thumbnail.py
+++ trunk/scripts/blender_thumbnail.py
@@ -28,7 +28,7 @@ class BBox:
         self.x1 = self.x2 = None
         self.y1 = self.y2 = None
         self.z1 = self.z2 = None    
-        
+
         if bbox:                
             for v in bbox: 
                 self.x1 = min(self.x1, v.x) or v.x
@@ -86,7 +86,7 @@ class BBox:
         return self.x1 and self.x2 and \
                self.y1 and self.y2 and \
                self.z1 and self.z2
-    
+
     def join(self, bbox):
         self.x1 = min(self.x1, bbox.x1) or bbox.x1
         self.x2 = max(self.x2, bbox.x2) or bbox.x2
@@ -103,7 +103,7 @@ class BBox:
                 self.y1, self.y2,
                 self.z1, self.z2)
 
-def render_thumbnail(axis, resolution, outfile):
+def render_thumbnail(longitude, latitude, rotation, resolution, outfile):
     total = BBox()
 
     scn = Blender.Scene.GetCurrent()
@@ -116,95 +116,41 @@ def render_thumbnail(axis, resolution, outfile):
             # print obj.getType()
             # print obj.boundingBox
             total.join(BBox(obj.boundingBox))
-    
+
     if not total.valid:
         # Scene is empty
         pass
     else:
-        # print total
         # Position of camera and support objects
 
-        if axis == "x": # ok
-            (x, y, z) = (total.x1 - 5,
-                         (total.y2 + total.y1)/2,
-                         (total.z2 + total.z1)/2)
-            euler = Euler(-math.pi/2, math.pi, math.pi/2)
-        elif axis == "-x": # ok
-            (x, y, z) = (total.x2 + 5,
-                         (total.y2 + total.y1)/2,
-                         (total.z2 + total.z1)/2)
-            euler = Euler(math.pi/2, 0, math.pi/2)
-        elif axis == "y":
-            (x, y, z) = ((total.x2 + total.x1)/2,
-                         total.y1 - 5,
-                         (total.z2 + total.z1)/2)
-            euler = Euler(math.pi/2, 0, 0)     
-        elif axis == "-y":
-            (x, y, z) = ((total.x2 + total.x1)/2,
-                         total.y2 + 5,
-                         (total.z2 + total.z1)/2)
-            euler = Euler(-math.pi/2, math.pi, 0)    
-        elif axis == "z": # ok
-            (x, y, z) = ((total.x2 + total.x1)/2,
-                         (total.y2 + total.y1)/2,
-                         total.z1 - 5)
-            euler = Euler(0, math.pi, 0)
-        elif axis == "-z": # ok
-            (x, y, z) = ((total.x2 + total.x1)/2,
-                         (total.y2 + total.y1)/2,
-                         total.z2 + 5)
-            euler = Euler(0, 0, 0)
-        else:
-            raise "Unknown axis: '%s'" % (axis,)
-        
         ### Add camera
         cam = Camera.New('ortho')
         # cam.scale += cam.scale * 0.1
         cam_obj = scn.objects.new(cam)
         scn.setCurrentCamera(cam_obj)
-         
+
         ### Add lamp
         light = Lamp.New('Sun')            # create new 'Spot' lamp data
         light.energy = 1.0
         # light.setMode('Square', 'Shadow')   # set these two lamp mode flags
         light_obj = scn.objects.new(light)
 
-        if False: # 3/4 perspective test
-            cam.scale = total.max_diagonal()
-            matrix = RotationMatrix(90, 4, 'x') * \
-                     TranslationMatrix(Vector(0, -total.max_diagonal(),0)) * \
-                     RotationMatrix(-45, 4, 'x') * \
-                     RotationMatrix(-45*3, 4, 'z') * \
-                     TranslationMatrix(total.center())
-            cam_obj.setMatrix(matrix)
-            light_obj.setMatrix(matrix)
-        else:
-            cam.scale = total.max()
-            cam_obj.setLocation(x, y, z)
-            cam_obj.setEuler(euler)
-            light_obj.setLocation(x, y, z)
+        cam.scale = total.max_diagonal()
+        matrix = RotationMatrix(rotation, 4, 'z') * \
+                 RotationMatrix(90, 4, 'x') * \
+                 TranslationMatrix(Vector(0, -total.max_diagonal()-2,0)) * \
+                 RotationMatrix(latitude,  4, 'x') * \
+                 RotationMatrix(longitude, 4, 'z') * \
+                 TranslationMatrix(total.center())
+        cam_obj.setMatrix(matrix)
+        light_obj.setMatrix(matrix)
 
         render = scn.getRenderingContext()
 
         render.renderwinSize = 100
-        
+
         render.aspectX = 100
         render.aspectY = 100
-
-#         if axis == "x" or axis == "-x": # front
-#             lmax = total.max()  #max(total.width_y(), total.width_z())
-#             render.sizeX = int(resolution * total.width_y() / lmax)
-#             render.sizeY = int(resolution * total.width_z() / lmax)
-#         elif axis == "y" or axis == "-y": # side
-#             lmax = total.max() # max(total.width_x(), total.width_z())
-#             render.sizeX = int(resolution * total.width_x() / lmax)
-#             render.sizeY = int(resolution * total.width_z() / lmax)
-#         elif axis == "z" or axis == "-z": # top
-#             lmax = total.max() # max(total.width_y(), total.width_x())
-#             render.sizeX = int(resolution * total.width_x() / lmax)
-#             render.sizeY = int(resolution * total.width_y() / lmax)
-#         else:
-#             raise "Unknown axis: '%s'" % (axis,)
 
         render.sizeX = resolution
         render.sizeY = resolution
@@ -225,8 +171,18 @@ def render_thumbnail(axis, resolution, outfile):
 
         # print "total: %s" % total
         Blender.Quit()
-        
-render_thumbnail(os.getenv("BLEND_THUMB_AXIS") or "x",
+
+# longitude: rotations around z-axis
+# latitude:  rotations around x/y-axis
+# rotation:  rotates the final image
+
+pos = os.getenv("BLEND_THUMB_POS") or "0,0,0"
+(longitude, latitude, rotation) = pos.split(',')
+longitude = -int(longitude) + -90
+latitude  = -int(latitude)
+rotation  =  int(rotation)
+
+render_thumbnail(longitude, latitude, rotation,
                  int(os.getenv("BLEND_THUMB_RESOLUTION") or 512),
                  os.getenv("BLEND_THUMB_OUTPUT") or "/tmp/out.png")
 
